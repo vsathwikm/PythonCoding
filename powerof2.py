500f7cd065c2b694dd7ef1a82606977b9679fd22
Vangari Sathwik Mohan <sathwik.mohan@gmail.com>
2017-06-22 12:03:41 -0400

    Update powerof2.py

--- powerof2.py
+++ powerof2.py
@@ -1,7 +1,11 @@
 # Simple program to find out if a number (integer or decimal) is a power of 2 or not
+# initializing an input array
 x = [4,64,-5,65,0.25,0.5,1]
+
 for i in x:
     num = float(i)
+    
+    # Case if input number is greater than 1, i.e. for positive powers of 2
     if num>1:
         if num%2 == 1:
             print ("0\n")
@@ -13,8 +17,12 @@ for i in x:
                 print ("1\n")
             else:
                 print ("0\n")
+    
+    # Case if power of 2 is zero
     elif num == 1:
         print ("1\n")
+    
+    # Case if input number is less than 1, i.e. for negative powers of 2
     else:
         while num<1 and num>0:
             num = num*2
